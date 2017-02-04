bfa312c7fe08217946c95d92bf5c26c29317af9b
apirani <adam.pirani@mlb.com>
2017-02-04 14:46:00 -0500

    Add back in url.txt file usage

--- harvesting-toolkit/tools/url-archiver/save-to-ia.sh
+++ harvesting-toolkit/tools/url-archiver/save-to-ia.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+url=$(awk -F = '{print $1}' url.txt)
 mkdir -p data
 for i in $(cat paths.txt);
 do 
-	# echo "${i}"
-	curl "http://web.archive.org/save/${i}"
+	# echo "${url}${i}"
+	curl "http://web.archive.org/save/${url}${i}"
 	sleep 2
 done
